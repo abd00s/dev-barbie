cbd31195fcbc1a4fcee4e088fe621be9bd655fbf
Abdullah <abd00s@users.noreply.github.com>
2016-05-08 15:09:56 -0400

    Prep numbers of clicks from last 7 days

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
     @clicks = Click.all
+    @clicks_from_today = Click.count_from(Date.today)
+    @clicks_from_1_day_ago = Click.count_from(Date.today - 1)
+    @clicks_from_2_days_ago = Click.count_from(Date.today - 2)
+    @clicks_from_3_days_ago = Click.count_from(Date.today - 3)
+    @clicks_from_4_days_ago = Click.count_from(Date.today - 4)
+    @clicks_from_5_days_ago = Click.count_from(Date.today - 5)
+    @clicks_from_6_days_ago = Click.count_from(Date.today - 6)
   end
 end
