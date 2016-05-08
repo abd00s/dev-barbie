2f15b46dd47632f42a8f92e35d65065bdc01ec9e
Abdullah <abd00s@users.noreply.github.com>
2016-05-08 19:05:04 -0400

    Generate chart of clicks per last week

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,55 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function()  {
+  $('#chart').highcharts({
+    chart: {
+      type: "column"
+    },
+    title: {
+      text: "Clicks by Day"
+    },
+    xAxis: {
+      categories: [
+          $('#chart').data('daym6'),
+          $('#chart').data('daym5'),
+          $('#chart').data('daym4'),
+          $('#chart').data('daym3'),
+          $('#chart').data('daym2'),
+          $('#chart').data('daym1'),
+          'Today'
+      ],
+      crosshair: true
+    },
+    yAxis: {
+      min: 0,
+      title: {
+          text: 'Clicks'
+      }
+    },
+    series: [{
+        name: 'Clicks',
+        colorByPoint: true,
+        data: [{
+            name: $('#chart').data('daym6'),
+            y: $('#chart').data('countm6'),
+        }, {
+            name: $('#chart').data('daym5'),
+            y: $('#chart').data('countm5'),
+        }, {
+            name: $('#chart').data('daym4'),
+            y: $('#chart').data('countm4'),
+        }, {
+            name: $('#chart').data('daym3'),
+            y: $('#chart').data('countm3'),
+        }, {
+            name: $('#chart').data('daym2'),
+            y: $('#chart').data('countm2'),
+        }, {
+            name: $('#chart').data('daym1'),
+            y: $('#chart').data('countm1'),
+        }, {
+            name: 'Today',
+            y: $('#chart').data('today'),
+        }]
+    }]
+
+  });
+});
--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -3,7 +3,7 @@ class ClicksController < ApplicationController
     @click = current_user.clicks.build
     respond_to do |format|
       if @click.save
-        ClickMailer.clicked_email(current_user, @click).deliver_now
+        # ClickMailer.clicked_email(current_user, @click).deliver_now
         format.html {redirect_to root_path, notice: 'One more shrimp on the barbie!'}
       else
         format.html {redirect_to root_path, alert: 'Something went wrong'}
