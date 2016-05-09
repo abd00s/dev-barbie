d96957f674f5d62db90585abe849985818eb9b3e
Abdullah <abd00s@users.noreply.github.com>
2016-05-09 13:12:04 -0400

    Handle autho and authen in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  authorize_resource :class => ApplicationController, :unless => :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, :alert => exception.message
+  end
 end
