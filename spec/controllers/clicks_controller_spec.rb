d32fdf352370153bc3ec52e1dec04ce7fcbbc29b
Abdullah <abd00s@users.noreply.github.com>
2016-05-10 14:08:14 -0400

    Spec clicks controller

--- spec/controllers/clicks_controller_spec.rb
+++ spec/controllers/clicks_controller_spec.rb
@@ -7,7 +7,11 @@ describe  "ClicksController" do
 
   describe "#create Action" do
     it "Increases the total number of clicks by one" do
-      expect { :click }.to change {Click.count}.by(1)
+      expect { Fabricate(:click, user: user) }.to change {Click.count}.by(1)
+    end
+
+    it "Associates the click to the clicking user" do
+      expect(click.user_id).to eq(user.id)
     end
   end
 end
