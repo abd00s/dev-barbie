7c8d7a6ae31e08efb523a90623e09873d8dfa11c
Abdullah <abd00s@users.noreply.github.com>
2016-05-09 21:42:39 -0400

    More timezone fixes

--- app/helpers/clicks_helper.rb
+++ app/helpers/clicks_helper.rb
@@ -1,5 +1,5 @@
 module ClicksHelper
   def pretty_date(click)
-    click.created_at.strftime("%A %b %e at %I:%M %p")
+    click.created_at.in_time_zone("Eastern Time (US & Canada)").strftime("%A %b %e at %I:%M %p")
   end
 end
