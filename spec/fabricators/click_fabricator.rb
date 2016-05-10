a465a26f1d5c70c0092a73feb2cd5c8cd7459985
Abdullah <abd00s@users.noreply.github.com>
2016-05-10 09:52:15 -0400

    Timezone fix

--- spec/fabricators/click_fabricator.rb
+++ spec/fabricators/click_fabricator.rb
@@ -1,3 +1,3 @@
 Fabricator(:click) do
-  created_at { Faker::Date.between(6.days.ago, Date.current) }
+  created_at { Faker::Date.between(6.days.ago, Date.current).in_time_zone("Eastern Time (US & Canada)") }
 end
