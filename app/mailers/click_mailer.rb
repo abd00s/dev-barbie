29686b74a68ac00eb915a5cc43e0e84e2f8806bd
Abdullah <abd00s@users.noreply.github.com>
2016-05-09 21:24:15 -0400

    Extract target email to secrets, set up pw reset from address

--- app/mailers/click_mailer.rb
+++ app/mailers/click_mailer.rb
@@ -1,10 +1,9 @@
 class ClickMailer < ApplicationMailer
   helper :clicks
-  # include SendGrid
 
   def clicked_email(user, click)
     @click = click
     @user = user
-    mail(to: "abdullah.pharaon@gmail.com", subject: 'Another shrimp on the dev barbie')
+    mail(to: Rails.application.secrets.target_email, subject: 'Another shrimp on the dev barbie')
   end
 end
